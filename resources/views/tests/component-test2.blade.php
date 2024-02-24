dbfb69e40aeb641d9f06e942833358e13485a998
sarachan-white <sarachan-white-toy-poodle@outlook.jp>
2024-02-24 21:59:01 +0900

    classBase

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,1 +1,7 @@
-コンポーネントテスト２
+<x-tests.app>
+  <x-slot name="header">ヘッダー２</x-slot>
+  コンポーネントテスト２
+  <x-test-class-base classBaseMessage="メッセージ" />
+  <div class="mb-4"></div>
+  <x-test-class-base classBaseMessage="メッセージ" defaultMessage="初期値から変更しています" />
+</x-tests.app>
