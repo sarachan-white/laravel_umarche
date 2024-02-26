a1a5db6b25bf33c255408a1028a122b2190b4222
sarachan-white <sarachan-white-toy-poodle@outlook.jp>
2024-02-26 23:04:36 +0900

    ownersDelete

--- routes/admin.php
+++ routes/admin.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
 use App\Http\Controllers\Admin\OwnersController;
+use App\Models\Owner;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,6 +29,12 @@ Route::get('/', function () {
 
 Route::resource('owners', OwnersController::class)
   ->middleware('auth:admin');
+
+Route::prefix('expired-owners')
+  ->middleware('auth:admin')->group(function () {
+    Route::get('index', [OwnersController::class, 'expiredOwnerIndex'])->name('expired-owners.index');
+    Route::post('destroy/{owner}', [OwnersController::class, 'expiredOwnerDestroy'])->name('expired-owners.destroy');
+  });
 
 Route::get('/dashboard', function () {
   return view('admin.dashboard');
