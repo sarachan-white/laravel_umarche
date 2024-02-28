fc4e17d3c413e4ef16666a85b498a951c8a1146a
sarachan-white <sarachan-white-toy-poodle@outlook.jp>
2024-02-29 00:01:28 +0900

    shop edit,update

--- resources/views/owner/shops/index.blade.php
+++ resources/views/owner/shops/index.blade.php
@@ -10,6 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="w-1/2 p-4">
+                        <x-flash-message status="session('status')" />
                     @foreach ($shops as $shop)
                         <a href="{{ route('owner.shops.edit', ['shop' => $shop->id]) }}">
                             <div class="border rounded-md p-4">
@@ -21,13 +22,7 @@
                                     @endif
                                 </div>
                                 <div class="text-xl">{{ $shop->name }}</div>
-                                <div>
-                                    @if(empty($shop->filename))
-                                    <img src="{{ asset('images/no_image.jpg') }}">
-                                    @else
-                                    <img src="{{ asset('storage/shops/' . $shop->filename) }}">
-                                    @endif
-                                </div>
+                                <x-shop-thumbnail :filename="$shop->filename" />
                             </div>
                         </a>
                         </div>
