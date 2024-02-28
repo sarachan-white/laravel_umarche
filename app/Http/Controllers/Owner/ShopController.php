1284e48515acec60d4acae0e0dcbc86cf3d0f486
sarachan-white <sarachan-white-toy-poodle@outlook.jp>
2024-02-28 17:20:37 +0900

    リサイズなしアップロード

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Shop;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class ShopController extends Controller
 {
@@ -38,10 +39,20 @@ class ShopController extends Controller
 
     public function edit($id)
     {
-        dd(Shop::findOrFail($id));
+        $shop = Shop::findOrFail($id);
+        // dd(Shop::findOrFail($id));
+        return view('owner.shops.edit', compact('shop'));
     }
 
     public function update(Request $request, $id)
     {
+        $imageFile = $request->image;
+        //選択されているか＆アップロードできているか
+        if (!is_null($imageFile) && $imageFile->isValid()) {
+            //フォルダー、ファイル名を作成
+            Storage::putFile('public/shops', $imageFile);
+        }
+
+        return redirect()->route('owner.shops.index');
     }
 }
